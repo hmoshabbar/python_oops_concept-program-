831e56e7df85000bfb09c6f41cec072075ebf037
Moshabbar Hussain <talktomoshabbar@gmail.com>
2017-08-03 21:35:36 +0530

    Class Explmple 1

---
+++ class_and_object1.py
@@ -0,0 +1,39 @@
+# create a class:
+# Class:: Class is a blue Print of which object we are created.
+
+
+class Student:    # its a class 
+    pass
+
+
+stu_1=Student()   # its a object of Student Class
+stu_2=Student()   # its a object of Student Class
+
+# Now he have to create instance Variable for object....(for obj 1)
+# instance Variable: is a variable we are create for object
+
+stu_1.first="Moshabbar"
+stu_1.last="Hussain"
+stu_1.email="moshabbar.hussain@gmail.com"
+stu_1.marks=60
+
+# Now he have to create instance Variable for object....(for obj 1)
+
+stu_2.first="Rahul"
+stu_2.last="Amin"
+stu_2.email="rahul.amin@gmail.com"
+stu_2.marks=98
+
+# Now Print Statement use for display result purpose(for stu_1)
+print stu_1.first.
+print stu_1.last
+print stu_1.email
+print stu_1.marks
+
+# Now Print Statement use for display result purpose(for stu_2)
+print stu_2.first
+print stu_2.last
+print stu_2.email
+print stu_2.marks
+
+
